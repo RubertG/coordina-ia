f696ae5a9d8a215bc0f8ae4d3d37d01b8ad3b719
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-11-07 00:27:55 -0500

    Fix: footer styles and contend

--- src/modules/core/components/footer.tsx
+++ src/modules/core/components/footer.tsx
@@ -2,9 +2,9 @@ import Link from "next/link"
 
 export const Footer = () => {
   return (
-    <footer className="mx-auto max-w-4xl space-y-8 px-4 lg:px-0 text-sm">
+    <footer className="mx-auto my-4 text-sm">
       <p className="text-center text-sm text-zinc-500">
-        Diseñado y desarrollado por
+        Diseñado por
         <Link
           target="_blank"
           className="hover:underline lg:transition-colors lg:hover:text-zinc-700"
