9304430acf68b114372eaab09a7d36f45bc1d710
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-11-06 23:53:49 -0500

    New: app Background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="es">
       <body
-        className={`${poppins.className} bg-zinc-950 antialiased`}
+        className={`${poppins.className} bg-zinc-950 dark:bg-zinc-50 antialiased`}
       >
         {children}
       </body>
