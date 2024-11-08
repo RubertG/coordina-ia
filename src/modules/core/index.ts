499bb237e133254ff4e2b0164f061a290e3fb670
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-11-08 17:59:09 -0500

    new: imports container

--- src/modules/core/index.ts
+++ src/modules/core/index.ts
@@ -1,6 +1,11 @@
 export * from "./fonts/poppins"
 
 export * from "./lib/utils"
+export * from "./lib/supabase/client"
+export * from "./lib/supabase/server"
+export * from "./lib/supabase/middleware"
+
+export * from "./types/types"
 
 export * from "./components/footer"
 export * from "./components/nav"
