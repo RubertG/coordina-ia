067a44881558cd1edf81412adabeb1aaaa591896
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-11-09 15:45:17 -0500

    fix: funtion by save user in User table in sigin

--- src/modules/auth/actions/login.ts
+++ src/modules/auth/actions/login.ts
@@ -8,7 +8,23 @@ import { registerSchemaType } from '@/modules/auth'
 
 export async function login(data: registerSchemaType) {
   const supabase = await createClient()
-  const { error, data: { user } } = await supabase.auth.signInWithPassword(data)
+  const { error } = await supabase.auth.signInWithPassword(data)
+
+  console.log(error)
+
+  if (error) {
+    return {
+      error: "Error al iniciar sesión"
+    }
+  }
+
+  revalidatePath('/', 'layout')
+  redirect('/')
+}
+
+export async function signup(data: registerSchemaType) {
+  const supabase = await createClient()
+  const { error, data: { user } } = await supabase.auth.signUp(data)
 
   console.log(error)
 
@@ -19,23 +35,8 @@ export async function login(data: registerSchemaType) {
   }
 
   if (user) {
-    await supabase.from('Usuario').insert([{ id: user.id, email: user.email }])
-  }
-
-  revalidatePath('/', 'layout')
-  redirect('/')
-}
-
-export async function signup(data: registerSchemaType) {
-  const supabase = await createClient()
-  const { error } = await supabase.auth.signUp(data)
-
-  console.log(error)
-
-  if (error) {
-    return {
-      error: "Error al iniciar sesión"
-    }
+    const res = await supabase.from('Usuario').insert([{ id: user.id, email: user.email }])
+    console.log(res)
   }
 
   revalidatePath('/', 'layout')
