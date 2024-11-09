56a0fe8a7596a397eec716a7207190bd99d29f15
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-11-09 17:33:28 -0500

    new: responsive nav

--- src/modules/core/components/nav-item.tsx
+++ src/modules/core/components/nav-item.tsx
@@ -5,7 +5,6 @@ import clsx from "clsx"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
-
 export const NavItem = ({
   href, label
 }: NavLink) => {
@@ -13,16 +12,14 @@ export const NavItem = ({
   const isActive = href === pathname
 
   return (
-    <li key={href}>
-      <Link
-        href={href}
-        className={clsx("block py-2 px-3 lg:transition-colors lg:hover:text-primary", {
-          "text-primary": isActive,
-          "text-zinc-900": !isActive
-        })}
-      >
-        {label}
-      </Link>
-    </li>
+    <Link
+      href={href}
+      className={clsx("block py-2 px-3 lg:transition-colors lg:hover:text-primary", {
+        "text-primary": isActive,
+        "text-zinc-900": !isActive
+      })}
+    >
+      {label}
+    </Link>
   )
 }
--- src/modules/core/components/nav.tsx
+++ src/modules/core/components/nav.tsx
@@ -1,6 +1,10 @@
+"use client"
+
 import Link from "next/link"
-import { Button, LoginButton, NavItem, NavLink } from "@/modules/core"
-import { Suspense } from "react"
+import { LoginButton, NavItem, NavLink } from "@/modules/core"
+import { useState } from "react"
+import { MenuIcon } from "lucide-react"
+import clsx from "clsx"
 
 const links: NavLink[] = [
   {
@@ -14,26 +18,50 @@ const links: NavLink[] = [
 ]
 
 export const Nav = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => setIsOpen((prev) => !prev)
+
   return (
     <nav className="bg-background shadow-lg shadow-zinc-200/30">
-      <nav className="mx-auto max-w-7xl px-4 py-3 flex items-center justify-between">
-        <Link
-          href="/"
-          className="lg:transition-colors lg:hover:text-primary/90 font-black text-xl lg:text-3xl text-primary"
-        >
-          Coordina IA
-        </Link>
+      <nav className="mx-auto max-w-7xl px-4 py-3 flex flex-col md:flex-row md:items-center justify-between relative">
+        <div className="flex items-center justify-between md:block">
+          <Link
+            href="/"
+            className="lg:transition-colors lg:hover:text-primary/90 font-black text-xl lg:text-3xl text-primary"
+          >
+            Coordina IA
+          </Link>
 
-        <ul className="flex items-center">
+          <button
+            className="md:hidden top-3 right-3"
+            onClick={toggleMenu}
+            aria-expanded={isOpen}
+            aria-controls="navigation-menu"
+          >
+            <MenuIcon className="w-5 h-5" />
+          </button>
+        </div>
+
+        <ul className={clsx(
+          "absolute md:static top-10 left-0 w-full md:w-auto bg-background shadow-zinc-200/30 md:bg-transparent md:shadow-none flex items-center flex-col md:flex-row py-5 md:py-0 transition-all",
+          {
+            "opacity-0 -translate-x-full md:translate-x-0 md:opacity-100": !isOpen,
+            "opacity-100 translate-x-0": isOpen
+          }
+        )}>
           {
             links.map(({ href, label }) => (
-              <NavItem key={href} href={href} label={label} />
+              <li
+                key={href}
+                onClick={toggleMenu}
+              >
+                <NavItem href={href} label={label} />
+              </li>
             ))
           }
-          <li className="ml-3">
-            <Suspense fallback={<Button variant="outline">Cargando...</Button>}>
-              <LoginButton />
-            </Suspense>
+          <li className="mt-3 md:mt-0 md:ml-3">
+            <LoginButton />
           </li>
         </ul>
       </nav>
