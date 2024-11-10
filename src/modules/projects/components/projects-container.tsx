66075e6f5b265f1d95a2ca40de63828e76b11f56
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-11-10 10:49:15 -0500

    fix: gap

--- src/modules/projects/components/projects-container.tsx
+++ src/modules/projects/components/projects-container.tsx
@@ -7,7 +7,7 @@ export const ProjectsContainer = ({
   children, className
 }: Props) => {
   return (
-    <ul className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 items-start ${className}`}>
+    <ul className={`grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-2 lg:gap-3 items-start ${className}`}>
       {children}
     </ul>
   )
