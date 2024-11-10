01e0b0bc5b4480f1be5c7fe2f95307c63c07874a
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-11-10 12:33:02 -0500

    new: Create poject page

--- src/app/crear-proyecto/page.tsx
+++ src/app/crear-proyecto/page.tsx
@@ -1,8 +1,15 @@
+import { CreateProjectForm } from "@/modules/projects"
+
 function CreateProjectPage() {
   return (
-    <div>
-      Página de crear proyecto
-    </div>
+    <>
+      <h1 className="text-3xl lg:text-4xl font-extrabold text-center text-primary">
+        Crear proyecto
+      </h1>
+      <section className="flex flex-wrap justify-center gap-4 mt-8">
+        <CreateProjectForm className="max-w-xl" />
+      </section>
+    </>
   )
 }
 
