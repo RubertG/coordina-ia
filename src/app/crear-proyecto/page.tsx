4301fda1b664216684631f8075e6cc19394f109c
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-12-14 13:18:08 -0500

    simplificar la estructura del componente de creación de proyecto

--- src/app/crear-proyecto/page.tsx
+++ src/app/crear-proyecto/page.tsx
@@ -6,9 +6,7 @@ function CreateProjectPage() {
       <h1 className="text-3xl lg:text-4xl font-extrabold text-center text-primary">
         Crear proyecto
       </h1>
-      <section className="flex flex-wrap justify-center gap-4 mt-8">
-        <CreateProjectForm className="max-w-xl" />
-      </section>
+      <CreateProjectForm className="mt-8" />
     </>
   )
 }
