3c318862ed5b29b9abec262bb7f401af306d42fe
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-12-14 13:18:24 -0500

    new: agregar componente WorkersModal para seleccionar trabajadores en proyectos

---
+++ src/modules/projects/components/workers-modal.tsx
@@ -0,0 +1,69 @@
+import { ReactNode } from "react"
+import { SelectedWorker } from "../types/types"
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle
+} from "@/modules/core"
+
+interface Props {
+  className?: string
+  loading?: boolean
+  workers: SelectedWorker[]
+  error?: string
+  open: boolean
+  setOpen: (open: boolean) => void
+
+  onLoading?: () => ReactNode
+  children: (workers: SelectedWorker[]) => ReactNode
+}
+
+const WorkersModal = ({
+  className,
+  loading,
+  workers,
+  error,
+  onLoading,
+  children,
+  setOpen,
+  open
+}: Props) => {
+  return (
+    <AlertDialog
+      open={open}
+      onOpenChange={setOpen}
+    >
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>Selecciona los trabajadores que quieres</AlertDialogTitle>
+          <AlertDialogDescription>
+            Puedes elegir a los trabajadores que quieras para tu proyecto. Esta recomendación es hecha por IA. Los trabajadores están compuestos por su <strong>nombre</strong> y la <strong>cantidad de trabajos</strong> que han realizado.
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+
+        <div className={`grid max-h-96 overflow-y-auto ${className}`}>
+          {loading && onLoading?.()}
+
+          {
+            error && (
+              <p>{error}</p>
+            )
+          }
+          {(!loading && workers.length !== 0 && !error) && children(workers)}
+        </div>
+
+        <AlertDialogFooter>
+          <AlertDialogAction>
+            Aceptar
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  )
+}
+
+export default WorkersModal
