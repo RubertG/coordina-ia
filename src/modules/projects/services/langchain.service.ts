4c93decdeb5e184501dbbb93939fec2b12d1a2d9
Rubert Gonzalez <rubertgonzalez5@gmail.com>
2024-12-17 17:34:27 -0500

    feat: actualizar LangChainService para aceptar parámetros de creación de proyectos

--- src/modules/projects/services/langchain.service.ts
+++ src/modules/projects/services/langchain.service.ts
@@ -1,11 +1,17 @@
 'use server'
-import { ChatGoogleGenerativeAI } from '@langchain/google-genai'
-import { ChatPromptTemplate } from '@langchain/core/prompts'
-import { JsonOutputParser } from '@langchain/core/output_parsers'
-import { WorkersService } from '@/modules/workers'
-import { ProjectsService } from './projects.service'
 
-export async function LangChainService() {
+import { WorkersService } from '@/modules/workers'
+import { JsonOutputParser } from '@langchain/core/output_parsers'
+import { ChatPromptTemplate } from '@langchain/core/prompts'
+import { ChatGoogleGenerativeAI } from '@langchain/google-genai'
+
+import { ProjectCreationSchema } from '../types/types'
+
+export async function LangChainService({
+  description,
+  maxWorkers,
+  technologies,
+}: ProjectCreationSchema): Promise<string[]> {
   const llm = new ChatGoogleGenerativeAI({
     model: 'gemini-1.5-flash',
     temperature: 0,
@@ -30,14 +36,13 @@ export async function LangChainService() {
   const chain = llm.pipe(parser)
 
   const wor = await WorkersService.getWorkers()
-  const pro = await ProjectsService.getProject('456e51e7-928b-4473-8183-b313da0d2747')
 
-  if (pro.data && wor.data) {
+  if (wor.data) {
     const rta = await chatTemplate.invoke({
-      techs: pro.data[0].tecnologias,
+      techs: technologies,
       workers: wor.data,
-      cant: 10,
-      desc: pro.data[0].descripcion,
+      cant: maxWorkers,
+      desc: description,
     })
 
     rawData = await chain.invoke(rta)
