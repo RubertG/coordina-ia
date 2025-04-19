7bbd64d052610206f1f3492c4a8f332459e19fb2
Rubert <rubert@4thewords.com>
2025-04-18 19:53:57 -0500

    feat: agregar componente WorkerKeyPointsModal para mostrar detalles del trabajador

--- src/modules/projects/components/worker-item.tsx
+++ src/modules/projects/components/worker-item.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 /**
  * Componente para mostrar un trabajador individual en la lista de selección.
  *
@@ -5,9 +7,13 @@
  * @returns {JSX.Element} - Retorna un elemento JSX.
  */
 
+import { Button, Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from '@/modules/core'
 import clsx from 'clsx'
+import { Info, Minus, Plus } from 'lucide-react'
+import { useState } from 'react'
 
 import { Worker } from '../types/types'
+import { WorkerKeyPointsModal } from './worker-key-points-modal'
 
 interface Props {
   className?: string
@@ -17,18 +23,51 @@ interface Props {
 }
 
 export const WorkerItem = ({ className, worker, onClick, isSelected }: Props) => {
+  const [open, setOpen] = useState(false)
+
   return (
     <li
       className={clsx(
-        `flex cursor-pointer items-center justify-between border-b border-gray-200 p-2 last:border-b-0 lg:hover:bg-zinc-100 ${className}`,
+        `flex items-center justify-between border-b border-gray-200 p-2 transition-colors last:border-b-0 ${className}`,
         {
           'bg-zinc-200': isSelected,
         },
       )}
-      onClick={() => onClick(worker)}
     >
-      <p className="text-sm text-zinc-700">{worker.name}</p>
-      <p>{worker.numberOfJobs}</p>
+      <div>
+        <p className="text-sm font-semibold text-zinc-800">{worker.name}</p>
+        <p className="text-xs text-zinc-700">Trabajos: {worker.numberOfJobs}</p>
+      </div>
+
+      <div>
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button size="icon" variant="ghost" onClick={() => onClick(worker)}>
+                {isSelected ? <Minus /> : <Plus />}
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>{isSelected ? 'Eliminar del' : 'Añadir al'} proyecto</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+
+        <TooltipProvider>
+          <Tooltip>
+            <TooltipTrigger asChild>
+              <Button size="icon" variant="ghost" onClick={() => setOpen(true)}>
+                <Info />
+              </Button>
+            </TooltipTrigger>
+            <TooltipContent>
+              <p>Puntos clave</p>
+            </TooltipContent>
+          </Tooltip>
+        </TooltipProvider>
+      </div>
+
+      <WorkerKeyPointsModal open={open} setOpen={setOpen} worker={worker} />
     </li>
   )
 }
